5d473553528d2251eb92a7047e0344bf628a0452
Leonard <2401091@sit.singaporetech.edu.sg>
2026-09-07 11:40:29 +0800

    printing variables

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("==================================")
 print("Welcome here")
 print("My first post!")
 print("==================================")
+
+username = "cool_creator tsss"
+bio = "Fun blogger test"
+followers = 10099
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
